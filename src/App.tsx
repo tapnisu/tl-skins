ca954b457503c331a82a7e9b4fbbfb63b03f0a1a
Aleksei Rybin <0xalekseirybin@gmail.com>
2024-02-18 20:23:26 +0700

    Added pico css & deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,51 @@
-import { useEffect, useState } from "react";
+import "@picocss/pico/css/pico.violet.min.css";
+import { useState } from "react";
+
+export interface TlData {
+  SKIN?: Skin;
+  CAPE?: Cape;
+}
+
+export interface Skin {
+  url: string;
+  metadata: Metadata;
+}
+
+export interface Metadata {
+  model: string;
+}
+
+export interface Cape {
+  url: string;
+  animatedCape?: boolean;
+  capeHeight?: number;
+  fps?: number;
+}
 
 function App() {
   const [username, setUsername] = useState("");
-  const [tlData, setTlData] = useState({});
+  const [tlData, setTlData] = useState<TlData>({});
 
-  useEffect(() => {
-    fetch(`http://localhost:8000/tlSkin/${"KitSuneHenaRu_"}`)
+  function fetchTlData() {
+    fetch(`http://localhost:8000/tlSkin/${username}`)
       .then((result) => result.json())
       .then((result) => setTlData(result));
-  }, [username]);
+  }
 
   return (
-    <div>
-      <img src={tlData.SKIN?.url} alt="" />
-      <input type="text" onChange={(e) => setUsername(e.target.value)} />
-    </div>
+    <main className="container">
+      <article>
+        <input
+          type="text"
+          placeholder="Введите username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input type="button" value="Загрузить" onClick={fetchTlData} />
+        <pre>
+          <img src={tlData.SKIN?.url} alt="Скин не найден" />
+        </pre>
+      </article>
+    </main>
   );
 }
 
